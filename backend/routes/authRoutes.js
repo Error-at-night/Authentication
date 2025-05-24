f777ae8ecfd2d62e3bdf16739da0e67f2774d1a6
Error-at-night <olayemisuccess553@gmail.com>
2025-05-25 00:11:50 +0100

    installed express-rate-limit and created a middleware for the resend-verification-code route to limit user request based on IP

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const { register, login, verifyEmail, resendVerificationCode, } = require("../controllers/authController")
 
+const resendVerificationCodeLimiter = require("../middleware/resend-verification-code-limiter")
+
 router.post("/register", register)
 router.post("/verify-email", verifyEmail)
-router.post("/resend-verification-code", resendVerificationCode)
+router.post("/resend-verification-code", resendVerificationCodeLimiter, resendVerificationCode)
 router.post("/login", login)
 
 module.exports = router
