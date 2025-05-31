951c4afb7e853a5656660b8c94c250d2156fb9b5
Error-at-night <olayemisuccess553@gmail.com>
2025-05-31 22:25:30 +0100

    Edited the message for sendResetPasswordEmail

--- backend/utils/sendResetPasswordEmail.js
+++ backend/utils/sendResetPasswordEmail.js
@@ -3,7 +3,9 @@ const sendEmail = require('./sendEmail');
 const sendResetPassswordEmail = async ({ name, email, token, origin }) => {
   const resetURL = `${origin}/reset-password/${token}`;
   const message = `<p>Please reset password by clicking on the following link : 
-  <a href="${resetURL}">Reset Password</a></p>`;
+    <a href="${resetURL}">Reset Password</a></p>
+    <p>This link will expire in 10 minutes</p>
+  `;
 
   return sendEmail({
     to: email,
