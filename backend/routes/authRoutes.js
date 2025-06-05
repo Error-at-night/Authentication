d5f893c4be50e3074b6ee019943234cf13da7f4a
Error-at-night <olayemisuccess553@gmail.com>
2025-06-05 01:36:23 +0100

    integrated logout api to the frontend, and created an authentication middleware and attach it to the backend logout route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,14 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 const { register, login, verifyEmail, resendVerificationCode, logout, forgotPassword, resetPassword, } = require("../controllers/authController")
-
 const { resendVerificationCodeRateLimiter, registerRateLimter, verifyEmailRateLimiter, loginRateLimiter } = require("../middleware/rate-limiter")
+const { authenticateUser } = require("../middleware/authentication")
 
 router.post("/register", registerRateLimter, register)
 router.post("/verify-email", verifyEmailRateLimiter, verifyEmail)
 router.post("/resend-verification-code", resendVerificationCodeRateLimiter, resendVerificationCode)
 router.post("/login", loginRateLimiter, login)
-router.delete("/logout", logout)
+router.delete("/logout", authenticateUser, logout)
 router.post("/forgot-password", forgotPassword)
 router.post("/reset-password/:token", resetPassword)
 
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -14,4 +14,5 @@ function Dashboard() {
     </div>
   )
 }
+
 export default Dashboard
